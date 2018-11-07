b4488a9685fc51675aa59fb7c86bc79290c8891a
msme <206696343@qq.com>
2018-11-07 17:53:46 +0800

    生产环境

--- webpack.common.js
+++ webpack.common.js
@@ -1,32 +1,25 @@
 const path = require('path')
 const HtmlWebpackPlugin = require('html-webpack-plugin')
 const CleanWebpackPlugin = require('clean-webpack-plugin')
-const webpack = require('webpack')
 
 module.exports = {
     entry: {
         app: './src/index.js',
     },
-    devtool: "inline-source-map",
-    devServer: {
-        contentBase: './dist',
-        hot: true
-    },
+    plugins: [
+        new CleanWebpackPlugin(['dist']),
+        new HtmlWebpackPlugin({
+            title: 'Production'
+        })
+    ],
     module: {
         rules: [
             {
-                test: /\.css$/,
+                test: /.css$/,
                 use: ['style-loader', 'css-loader']
             }
         ]
     },
-    plugins: [
-        new CleanWebpackPlugin(['dist']),
-        new HtmlWebpackPlugin({
-            title: 'Output Management'
-        }),
-        new webpack.HotModuleReplacementPlugin()
-    ],
     output: {
         filename: "[name].bundle.js",
         path: path.resolve(__dirname, 'dist')
